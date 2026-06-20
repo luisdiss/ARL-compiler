9a17f129381f9e5e6c62633cca1bc2423838f3d5
Luis <1luisdiss@gmail.com>
2026-06-20 02:09:15 +0100

    Update compiler.py

--- compiler.py
+++ compiler.py
@@ -6,9 +6,9 @@ from compiler.codegen.ir_builder import build_ir
 from compiler.codegen.arm64 import generate_arm64
 from compiler.lexer.token_utils import Token
 
-def compiler():
+def compiler() -> str:
     tokens = lexer(content)
     root = parser(tokens.append(Token("$", "$")))
     semantic_analyser(root)
     ir = build_ir(root)
-    asm = generate_arm64(ir)
+    return generate_arm64(ir)
